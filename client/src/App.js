fef60118916250a62b9ed02e691ed15950a39980
Manuel <manuel.brea99@gmail.com>
2020-07-08 21:20:43 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import NavigationBar from "./components/navigationBar";
+
+const App = () => (
+	<div>
+		<h1>Eureka Highland journal</h1>
+		<NavigationBar />
+	</div>
+);
+
+export default App;
---
+++ client/src/components/navigationBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class NavigationBar extends Component {
+	render() {
+		return (
+			<ul>
+				<li> Day1 </li>
+				<li> Day2 </li>
+			</ul>
+		);
+	}
+}
+
+export default NavigationBar;
