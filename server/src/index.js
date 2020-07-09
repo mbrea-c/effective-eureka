a4014740b066d280444e31766eb7be8a745351ed
Manuel <manuel.brea99@gmail.com>
2020-07-09 17:06:12 +0100

    Added first API endpoint on server to fetch index of posts

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,19 @@
 const Koa = require("koa");
-const app = new Koa();
+const Router = require("koa-router");
 const knex = require("knex")({
 	client: "pg",
 	connection: {
 		host: "127.0.0.1",
-		user: "manuel",
-		password: "",
+		user: "eureka_role",
+		password: "eureka",
 		database: "eureka"
 	}
 });
 
-app.use(async ctx => {
+const app = new Koa();
+const router = new Router();
+
+router.get("/", (ctx, next) => {
 	console.log(ctx.request);
 	ctx.body = "Hello world eureka";
 	knex("posts")
@@ -18,5 +21,14 @@ app.use(async ctx => {
 		.then(rows => console.log(rows));
 	console.log(ctx.response);
 });
+
+router.get("/getposts", async (ctx, next) => {
+	ctx.body = (await knex("posts").select("*")).map(row => {
+		row.link = `http://localhost:3000/posts/${row.id}`;
+		return row;
+	});
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000);
