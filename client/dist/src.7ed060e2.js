c7fbb57c8b65e65d4ff24c229ed2f2692cb4dee9
Sarah Ihme <sar.ihme@gmail.com>
2020-07-09 20:25:12 +0200

    Minor style changes

--- client/dist/src.7ed060e2.js
+++ client/dist/src.7ed060e2.js
@@ -28631,7 +28631,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34499" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41399" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
