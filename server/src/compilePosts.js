aa869a3fae1812af2781059cd661954044ade03d
Manuel <manuel.brea99@gmail.com>
2020-07-09 19:19:58 +0100

    Finished backend, testing pending

--- server/src/compilePosts.js
+++ server/src/compilePosts.js
@@ -3,6 +3,16 @@ const { exec } = require("child_process");
 
 module.exports = () => {
 	fs.readdir("./posts", (err, files) => {
-		files.map(file => console.log(file));
+		files.map(file => {
+			if (!fs.existsSync(`./public/posts/${file}`)) {
+				fs.mkdirSync(`./public/posts/${file}`);
+			}
+			exec(
+				`htlatex ../../../posts/${file}/index.tex html "" -d. "--interaction=nonstopmode"`,
+				{
+					cwd: `./public/posts/${file}/`
+				}
+			);
+		});
 	});
 };
